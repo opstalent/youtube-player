0dadaeb47f42adb63234cd1d41da2ffec1eaaaff
szymon.kunowski <szymon.kunowski@gmail.com>
2016-03-22 12:57:13 +0100

    Add configuration options

--- src/controllers/YoutubePlayerController.js
+++ src/controllers/YoutubePlayerController.js
@@ -12,13 +12,73 @@
 
 export class YoutubePlayerController {
 
+    var $scope;
+    var $interval;
+    var YT_event;
+
+    var progress = 0;
+    var duration = 0;
+    var interval = undefined;
+    var playerStatus = "NOT PLAYING";
+    var currentTime = 0;
+
     /**
      * @param $scope
      * @param $interval
      * @param YT_event
      */
     constructor($scope, $interval, YT_event) {
+        this.$scope = $scope;
+        this.$interval = $interval;
+        this.YT_event = YT_event;
 
+        $scope.$on(YT_event.STATUS_CHANGE, function (event, data) {
+            var player = data.player;
+
+            this.playerStatus = data.message;
+            this.currentTime = player.getCurrentTime();
+
+            this.showProgress(player);
+        });
+
+        $scope.$on(YT_event.PLAYER_READY, function (event, data) {
+            this.duration = data.duration;
+        });
+    }
+
+    sendControlEvent(ctrlEvent, args) {
+        if (args) {
+            this.$scope.$broadcast(ctrlEvent, args);
+        } else {
+            this.$scope.$broadcast(ctrlEvent);
+        }
+    }
+
+    move(event) {
+        var bar = event.target.className === 'progress-bar progress-bar-info'
+            ? event.target.parentNode
+            : event.target;
+
+        this.currentTime = event.offsetX / bar.offsetWidth * vm.duration;
+
+        this.progress = this.currentTime / vm.duration * 100;
+        this.sendControlEvent(this.YT_event.MOVE, {currentTime: this.currentTime});
+    }
+
+    showProgress(player) {
+        if (this.currentTime !== null) {
+            this.progress = this.currentTime / this.duration * 100;
+        }
+        if (this.playerStatus === 'PLAYING') {
+            this.interval = $interval(function () {
+                this.progress = player.getCurrentTime() / this.duration * 100;
+            }, 500);
+        } else {
+            if (angular.isDefined(this.interval)) {
+                $interval.cancel(this.interval);
+                this.interval = undefined;
+            }
+        }
     }
 }
 
